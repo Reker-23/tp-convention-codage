4515bacb167ba0bebb0b4a19f0e762284861743a
Adrien Hussenet <hussenea@3il.fr>
2024-11-19 15:58:38 +0100

    Ex 3

--- Exercice 3/exo3.java
+++ Exercice 3/exo3.java
@@ -1,1 +1,37 @@
-// a vous de jouer
+/*
+Cette classe décrit un animal
+ */
+
+public class animal {
+
+    public String nom;
+    public int age;
+
+    public void parler();
+
+}
+
+
+public class chien  extends {
+
+    @override
+    public void parler(){
+        System.out.println("Woof woof");
+    }
+
+    public void decrire(){
+        System.out.println("Type: chien, Nom: "+nom+", Âge: "+age+" ans");
+    }
+}
+
+public class chat extends animal {
+
+    @override
+    public void parler(){
+        System.out.println("Miaou");
+    }
+
+    public void decrire(){
+        System.out.println("Type: chat, Nom: "+nom+", Âge: "+age+" ans");
+    }
+}
