85d2119e0ece72412fcb931a8700360e7b0af001
Adrien Hussenet <hussenea@3il.fr>
2024-11-19 15:53:14 +0100

    Exo 1 2

--- Exercice 1/exo1.java
+++ Exercice 1/exo1.java
@@ -1,6 +1,17 @@
+/*
+Cette classe décrit un animal
+ */
+
 public class animal {
-public String nom;
-public int age;
-public void parler(){System.out.println("Je suis un animal.");}
-public void AfficherInfo(){System.out.println("Nom: "+nom+", Âge: "+age+" ans");}
+    
+    public String nom;
+    public int age;
+
+    public void parler(){
+        System.out.println("Je suis un animal.");
+    }
+
+    public void AfficherInfo(){
+        System.out.println("Nom: "+nom+", Âge: "+age+" ans");
+    }
 }
